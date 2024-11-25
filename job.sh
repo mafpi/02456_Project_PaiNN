aa1b8d1fab49d0a369f822924645191c5aa7f8bf
mafpi <mafpi@dtu.dk>
2024-11-25 16:49:50 +0100

    bash job

---
+++ job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#BSUB -q gpuv100
+#BSUB -J painn3layers
+#BSUB -n 4
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=8GB]"
+#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -W 10:00
+#BSUB -o painn3layers%J.out
+#BSUB -e painn3layers%J.err
+
+
+# Initialize Python environment
+hpcintrogpush
+
+python mininal_example.py
